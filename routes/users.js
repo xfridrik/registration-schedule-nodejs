fbeac3af16daefeb393f139e1b4ac26d6ea720fb
xfridrik <xfridrik@stuba.sk>
2021-11-03 20:45:28 +0100

    feat: add login & register pages

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,12 @@ const router = express.Router();
 router.get('/',function(req,res){
     res.render('index');
 });
+router.get('/login',function(req,res){
+    res.render('login');
+});
+router.get('/register',function(req,res){
+    res.render('register');
+});
 
 
 module.exports = router
