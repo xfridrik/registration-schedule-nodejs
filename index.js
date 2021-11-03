77fa4b4b8e3d01f73573dfc81cbf39e3d962d4b2
xfridrik <xfridrik@stuba.sk>
2021-11-03 19:10:12 +0100

    feat: add view engine pug

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
-//server start
+// set view engine - pug
+application.set('views', path.join(__dirname,'views'));
+application.set('view engine', 'pug');
+
+// server start
 app.listen(process.env.PORT||3000,function(){
     console.log('Server is running...');
 })
