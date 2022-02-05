6260d6371cb1305283d87049ee1e20c405d1077f
3DRIK <art3drik@gmail.com>
2022-02-05 12:45:13 +0100

    update: pwa

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,6 @@
-navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-    // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+// serviceworker registration
+navigator.serviceWorker.register('./serviceworker.js').then(function(registration) {
+    console.log('ServiceWorker registered: ', registration.scope);
 }, function(err) {
-    // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
+    console.log('ServiceWorker not registered: ', err);
 });
--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,14 +1,26 @@
-const cacheName = 'news-v1';
-const staticAssets = [
+/*************************
+ * Service worker for PWA
+ * References: https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker
+ *************************/
+
+const cacheName = 'team-reg-app';
+const files = [
     "/img/icon-192x192.png",
     "/img/icon-256x256.png",
     "/img/icon-384x384.png",
-    "/img/icon-512x512.png"
+    "/img/icon-512x512.png",
+    "/img/home.svg",
+    "/img/logout.svg",
+    "/img/plus.svg",
+    "/img/schedule.svg",
+    "/img/settings.svg",
+    "/img/team.svg",
 ];
+
 
 self.addEventListener('install', async e => {
     const cache = await caches.open(cacheName);
-    await cache.addAll(staticAssets);
+    await cache.addAll(files);
     return self.skipWaiting();
 });
 
@@ -16,31 +28,10 @@ self.addEventListener('activate', e => {
     self.clients.claim();
 });
 
-self.addEventListener('fetch', async e => {
-    const req = e.request;
-    const url = new URL(req.url);
-
-    if (url.origin === location.origin) {
-        e.respondWith(cacheFirst(req));
-    } else {
-        e.respondWith(networkAndCache(req));
-    }
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
-
-async function cacheFirst(req) {
-    const cache = await caches.open(cacheName);
-    const cached = await cache.match(req);
-    return cached || fetch(req);
-}
-
-async function networkAndCache(req) {
-    const cache = await caches.open(cacheName);
-    try {
-        const fresh = await fetch(req);
-        await cache.put(req, fresh.clone());
-        return fresh;
-    } catch (e) {
-        const cached = await cache.match(req);
-        return cached;
-    }
-}
