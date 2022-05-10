879e8883f477d0110211f587dc8c053f16ee62fd
xfridrik <xfridrik@stuba.sk>
2022-05-10 13:13:56 +0200

    fix: db

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@
  *   References: https://node-postgres.com/features/connecting
  ******************************************************/
 
-const tables="CREATE TABLE IF NOT EXISTS users (id bigserial PRIMARY KEY NOT NULL, name VARCHAR(200) not NULL, email VARCHAR(200) not NULL, password VARCHAR(200) not NULL, team bigint references teams(id) on delete set null, privileges VARCHAR(16) not NULL);" +
+const tables="CREATE TABLE IF NOT EXISTS teams (id bigserial PRIMARY KEY NOT NULL, name VARCHAR(100) NOT NULL UNIQUE, preferred_match int, league bigint not NULL);" +
+    "CREATE TABLE IF NOT EXISTS users (id bigserial PRIMARY KEY NOT NULL, name VARCHAR(200) not NULL, email VARCHAR(200) not NULL, password VARCHAR(200) not NULL, team bigint references teams(id) on delete set null, privileges VARCHAR(16) not NULL);" +
     "CREATE TABLE IF NOT EXISTS matches (id bigserial PRIMARY KEY NOT NULL, home bigint, guest bigint, round int not NULL, date date not NULL, league bigint);" +
-    "CREATE TABLE IF NOT EXISTS teams (id bigserial PRIMARY KEY NOT NULL, name VARCHAR(100) NOT NULL UNIQUE, preferred_match int, league bigint not NULL);" +
     "CREATE TABLE IF NOT EXISTS leagues (id bigserial PRIMARY KEY NOT NULL, name VARCHAR(100) NOT NULL UNIQUE, start_date_first date not NULL, start_date_second date not NULL, nteams int not NULL, opened BOOLEAN not NULL);"
 
 const conString = process.env.DATABASE_URL || "postgresql://postgres:root@localhost/teamreg";
